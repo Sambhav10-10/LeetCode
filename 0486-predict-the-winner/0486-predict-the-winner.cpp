5aadd5ba68e3dd53b9c06e8187645c5d23c3e000
Sambhav Koshta <koshtasambhav18@gmail.com>
2026-08-02 01:09:13 +0530

    Time: 0 ms (100%), Space: 10.1 MB (52.55%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,23 +1,33 @@
 class Solution {
 public:
+    vector<vector<int>> dp;
+
     int solve(int i, int j, vector<int>& nums, int check) {
         if (i > j) return 0;
 
+        if (dp[i][j] != -1) return dp[i][j];
+
         if (check % 2 == 0) { 
-            return max(nums[i] + solve(i + 1, j, nums, check + 1),
-                       nums[j] + solve(i, j - 1, nums, check + 1));
-        } else { 
-            return min(solve(i + 1, j, nums, check + 1),
-                       solve(i, j - 1, nums, check + 1));
+            return dp[i][j] = max(
+                nums[i] + solve(i + 1, j, nums, check + 1),
+                nums[j] + solve(i, j - 1, nums, check + 1)
+            );
+        } else {
+            return dp[i][j] = min(
+                solve(i + 1, j, nums, check + 1),
+                solve(i, j - 1, nums, check + 1)
+            );
         }
     }
 
     bool predictTheWinner(vector<int>& nums) {
-        int total = 0;
-        for (int x : nums)
-            total += x;
+        int n = nums.size();
+        dp.assign(n, vector<int>(n, -1));
 
-        int p1 = solve(0, nums.size() - 1, nums, 0);
+        int total = 0;
+        for (int x : nums) total += x;
+
+        int p1 = solve(0, n - 1, nums, 0);
         int p2 = total - p1;
 
         return p1 >= p2;
